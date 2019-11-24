527fa17ee2bff7ffc3cfc1496a00cf6d5e0e7e70
Wesley Brown <nipponkid7@gmail.com>
2019-11-23 20:55:19 -0500

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Wesley Brown on 11/23/19.
+//  Copyright © 2019 Wesley Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
