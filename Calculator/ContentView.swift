8d40f6b35770f234da5f184b257d50df15bc5161
Wesley Brown <nipponkid7@gmail.com>
2019-11-27 16:59:37 -0500

    Add Buttons for All Digits

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,34 +9,125 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var input = "0"
+    @State private var number = "0"
     
     var body: some View {
         VStack {
-            TextField("", text: $input)
+            Text(number)
+                .font(.system(size: 60))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                .multilineTextAlignment(.trailing)
             
-            Button(action: {
-                self.appendToNumberArea("1")
-            }) {
-                Text("1")
-                .padding()
+            HStack {
+                Button(action: {
+                    self.number += "7"
+                }) {
+                    Text("7")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                .frame(maxWidth: .infinity)
+                
+                Button(action: {
+                    self.number += "8"
+                }) {
+                    Text("8")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                
+                Button(action: {
+                    self.number += "9"
+                }) {
+                    Text("9")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
             }
-            .background(Color.gray)
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    self.number += "4"
+                }) {
+                    Text("4")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                .frame(maxWidth: .infinity)
+                
+                Button(action: {
+                    self.number += "5"
+                }) {
+                    Text("5")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                
+                Button(action: {
+                    self.number += "6"
+                }) {
+                    Text("6")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+            }
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    self.number += "1"
+                }) {
+                    Text("1")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                .frame(maxWidth: .infinity)
+                
+                Button(action: {
+                    self.number += "2"
+                }) {
+                    Text("2")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+                
+                Button(action: {
+                    self.number += "3"
+                }) {
+                    Text("3")
+                        .padding()
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(Color.gray)
+            }
         }
-    }
-    
-    private func appendToNumberArea(_ number: String) {
-        if (input == "0") {
-            // No leading zeroes
-            input = number
-        } else {
-            input += number
-        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 8 Plus"))
     }
 }
