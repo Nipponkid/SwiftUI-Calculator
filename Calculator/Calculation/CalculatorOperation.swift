a0d465955cc72edb261775dbf317224fbc9ca2f8
Wesley Brown <brown.5776@osu.edu>
2020-01-03 23:39:59 -0500

    Create CalculatorOperation Type

---
+++ Calculator/Calculation/CalculatorOperation.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorOperation.swift
+//  Calculator
+//
+//  Created by Wesley Brown on 1/3/20.
+//  Copyright © 2020 Wesley Brown. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorOperation {
+    let firstInput = 0
+    let secondInput = 0
+}
---
+++ CalculatorTests/CalculatorOperationTests.swift
@@ -0,0 +1,27 @@
+//
+//  OperationTests.swift
+//  CalculatorTests
+//
+//  Created by Wesley Brown on 1/3/20.
+//  Copyright © 2020 Wesley Brown. All rights reserved.
+//
+
+@testable import Calculator
+import XCTest
+
+final class CalculatorOperationTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func test_aNewOperationGivenNoInputsHasAFirstAndSecondInputOfZero() {
+        let operation = CalculatorOperation()
+        XCTAssertEqual(operation.firstInput, 0)
+        XCTAssertEqual(operation.secondInput, 0)
+    }
+}
